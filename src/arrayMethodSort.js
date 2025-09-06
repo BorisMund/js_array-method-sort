bea0c268ba8c7b25e72063131258aa893d56f3ee
Boris Mund <boris17527684@gmail.com>
2025-09-06 22:46:41 +0300

    new commit

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -3,9 +3,46 @@
 /**
  * Implement method Sort
  */
+// const rooms = [
+//   { name: 'kitchen', square: 25 },
+//   { name: 'living room', square: 18 },
+//   { name: 'balcony', square: 7 },
+//   { name: 'restRoom', square: 30 },
+// ];
+
 function applyCustomSort() {
-  [].__proto__.sort2 = function(compareFunction) {
-    // write code here
+  [].__proto__.sort2 = function (compareFunction) {
+    const arr = this;
+
+    const cmp =
+      compareFunction ??
+      ((a, b) => {
+        const A = String(a);
+        const B = String(b);
+
+        if (A < B) {
+          return -1;
+        }
+
+        if (A > B) {
+          return 1;
+        }
+
+        return 0;
+      });
+
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length - i - 1; j++) {
+        if (cmp(arr[j], arr[j + 1]) > 0) {
+          const temp = arr[j];
+
+          arr[j] = arr[j + 1];
+          arr[j + 1] = temp;
+        }
+      }
+    }
+
+    return arr;
   };
 }
 
